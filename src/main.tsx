8a6e78711841fea011cad2b178fa28751f8d4ad9
Matheus Franzoni <matheusfranzonief@gmail.com>
2021-12-08 08:38:34 -0300

    FinalizadoCssBase

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
 
-import './styles/global.css'
+import './styles/global.css';
 
 ReactDOM.render(
   <React.StrictMode>
